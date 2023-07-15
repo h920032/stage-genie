99ce051be566c677ed558fc8eadebe3d65d98ed7
dipsy <h920032@gmail.com>
2023-07-15 23:46:39 +0800

    modify

--- time-controller-plus.ino
+++ time-controller-plus.ino
@@ -1,15 +1,30 @@
 #include <EEPROM.h>
-#define LEDR 8
-#define LEDG 6
-#define LEDB 4
+#define RECODRD_LED 4
+#define TIMER_LED 8
+#define NORMAL_LED 6
+#define RELAY_LED 2
 #define KEY1 A2
 #define KEY2 A1
 #define KEY3 A4
 #define KEY4 A3
 #define jdq 11
+#define MAX_INDEX 900
+#define EEPROM_SIZE 1024
+
+void writeIntIntoEEPROM(int address, int number)
+{ 
+  EEPROM.write(address, number >> 8);
+  EEPROM.write(address + 1, number & 0xFF);
+}
+int readIntFromEEPROM(int address)
+{
+  return (EEPROM.read(address) << 8) + EEPROM.read(address + 1);
+}
 
 void setup() {
   Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  digitalWrite(3, 0);
   pinMode(5, OUTPUT);
   digitalWrite(5, 0);
   pinMode(7, OUTPUT);
@@ -21,20 +36,23 @@ void setup() {
 
   pinMode(jdq, OUTPUT);
   digitalWrite(jdq, 1);
-  pinMode(LEDR, OUTPUT);					//定义LED为输出引脚
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
+  pinMode(RECODRD_LED, OUTPUT);
+  pinMode(TIMER_LED, OUTPUT);
+  pinMode(NORMAL_LED, OUTPUT);
+  pinMode(RELAY_LED, OUTPUT);
   pinMode(KEY1, INPUT_PULLUP);
   pinMode(KEY2, INPUT_PULLUP);
   pinMode(KEY3, INPUT_PULLUP);
   pinMode(KEY4, INPUT_PULLUP);
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, HIGH);
+  digitalWrite(RECODRD_LED, HIGH);
+  digitalWrite(TIMER_LED, HIGH);
+  digitalWrite(NORMAL_LED, HIGH);
+  digitalWrite(RELAY_LED, HIGH);
   delay(1000);
-  digitalWrite(LEDR, 0);
-  digitalWrite(LEDG, 1);
-  digitalWrite(LEDB, 0);
+  digitalWrite(RECODRD_LED, 0);
+  digitalWrite(TIMER_LED, 0);
+  digitalWrite(NORMAL_LED, 1);
+  digitalWrite(RELAY_LED, 0);
 }
 
 unsigned long times[2] = {0, 0}; // 記錄運行時間
@@ -44,87 +62,70 @@ int state = 0;
 int index = 0;
 int order = 0;
 int value = 0;
+int end_index = 0;
 
 void loop()
 {
-  if (digitalRead(KEY1) == 0  || index >= 1024) // Clearning data
+  if (digitalRead(KEY1) == 0  || index >= MAX_INDEX) // Clearning data
   {
     delay(10);
-    if (digitalRead(KEY1) == 0  || index >= 1024)
+    if (digitalRead(KEY1) == 0  || index >= MAX_INDEX)
     {
-      Serial.println("key1");
-      if (state == 0)
+      unsigned long button_time_start = 0;
+      unsigned long button_time_end = 0;
+      button_time_start = millis();
+      button_time_end = millis();
+      while (digitalRead(KEY1) == 0 && (button_time_end - button_time_start) < 5000)
       {
-        for (int i = 0; i < 1024; i++) {
+        button_time_end = millis();
+      } 
+      Serial.println("key1");
+      if (state == 0 && ((button_time_end - button_time_start) >= 5000))
+      {
+        for (int i = 0; i < EEPROM_SIZE; i++) {
           EEPROM.write(i, 0);
         }
+        writeIntIntoEEPROM(EEPROM_SIZE - 3, 0);
         state = 1;
         index = 0;
         order = 0;
         value = 0;
-        digitalWrite(LEDR, 0);
-        digitalWrite(LEDG, 0);
-        digitalWrite(LEDB, 0);
+        digitalWrite(RECODRD_LED, 1);
+        digitalWrite(TIMER_LED, 0);
+        digitalWrite(NORMAL_LED, 0);
+        digitalWrite(RELAY_LED, 0);
         while (digitalRead(KEY1) == 0);
         while (digitalRead(KEY2) == 1);
         times[0] = millis();
         times[1] = millis();
-        timestart = millis();
       }
       else
       {
+        if(state = 1) {
+          if (index < MAX_INDEX) {
+            EEPROM.write(index, value);
+            index++;
+          }
+          writeIntIntoEEPROM(EEPROM_SIZE - 3, index);
+        }
         state = 0;
-        // EEPROM.write(index, value);
         order = 0;
         value = 0;
         index = 0;
-        digitalWrite(LEDR, 1);
-        digitalWrite(LEDG, 0);
-        digitalWrite(LEDB, 0);
+        end_index = 0;
+        digitalWrite(RECODRD_LED, 0);
+        digitalWrite(TIMER_LED, 1);
+        digitalWrite(NORMAL_LED, 0);
+        digitalWrite(RELAY_LED, 0);
         while (digitalRead(KEY1) == 0);
-        digitalWrite(LEDR, 0);
-        digitalWrite(LEDG, 1);
-        digitalWrite(LEDB, 0);
+        digitalWrite(RECODRD_LED, 0);
+        digitalWrite(TIMER_LED, 0);
+        digitalWrite(NORMAL_LED, 1);
+        digitalWrite(RELAY_LED, 0);
       }
     }
-    // while (digitalRead(KEY1) == 0);
   }
-  if (state == 1 && millis() >= times[0]) //Learning
-  {
-    /*
-    times[0] = millis();
-    if ((millis() - times[1]) >= 1000) {
-      digitalWrite(LEDR, !digitalRead(LEDR));
-      times[1] = millis();
-    }
-    */
-    times[0] += 100;
-    if (millis() >= times[1]) {
-      digitalWrite(LEDR, !digitalRead(LEDR));
-      times[1] += 500;
-    }
-    if (digitalRead(KEY3) == 0)
-    {
-      // Serial.println("key3");
-      value = value ^ (1 << (7 - order));
-      // Serial.print(value);
-      digitalWrite(jdq, 0);
-      digitalWrite(LEDB, 1);
-    }
-    else
-    {
-      digitalWrite(jdq, 1);
-      digitalWrite(LEDB, 0);
-    }
-    order++;
-    if (order >= 8) {
-      EEPROM.write(index, value);
-      order = 0;
-      value = 0;
-      index++;
-    }
-  }
-  if (digitalRead(KEY2) == 0)
+  else if (digitalRead(KEY2) == 0)
   {
     delay(10);
     if (digitalRead(KEY2) == 0)
@@ -136,24 +137,56 @@ void loop()
         order = 0;
         index = 0;
         value = 0;
+        end_index = readIntFromEEPROM(EEPROM_SIZE - 3);
         times[0] = millis();
         times[1] = millis();
-        timestart = millis();
       }
+    }
+  }
+  else if (digitalRead(KEY4) == 0) // Dump data to serial
+  {
+    delay(10);
+    if (digitalRead(KEY4) == 0) {
+      Serial.println("key4");
+      for (int i = 0; i < MAX_INDEX; i++) {
+        Serial.print(EEPROM.read(i));
+        Serial.print(" ,");
+      }
+    }
+  }
+  if (state == 1 && millis() >= times[0]) //Learning
+  {
+    times[0] += 100;
+    if (millis() >= times[1]) {
+      digitalWrite(TIMER_LED, !digitalRead(TIMER_LED));
+      times[1] += 500;
+    }
+    if (digitalRead(KEY3) == 0)
+    {
+      // Serial.println("key3");
+      value = value ^ (1 << (7 - order));
+      // Serial.print(value);
+      digitalWrite(jdq, 0);
+      digitalWrite(RELAY_LED, 1);
+    }
+    else
+    {
+      digitalWrite(jdq, 1);
+      digitalWrite(RELAY_LED, 0);
+    }
+    order++;
+    if (order >= 8) {
+      EEPROM.write(index, value);
+      order = 0;
+      value = 0;
+      index++;
     }
   }
   if (state == 2  && millis() >= times[0]) // Trigger
   {
-    /*
-    times[0] = millis();
-    if ((millis() - times[1]) >= 1000) {
-      digitalWrite(LEDR, !digitalRead(LEDR));
-      times[1] = millis();
-    }
-    */
     times[0] += 100;
     if (millis() >= times[1]) {
-      digitalWrite(LEDR, !digitalRead(LEDR));
+      digitalWrite(TIMER_LED, !digitalRead(TIMER_LED));
       times[1] += 500;
     }
     if (order == 0) {
@@ -161,37 +194,27 @@ void loop()
     }
     if ((value >> (7 - order)) & 1) {
       digitalWrite(jdq, 0);
-      digitalWrite(LEDB, 1);
+      digitalWrite(RELAY_LED, 1);
     }
     else {
       digitalWrite(jdq, 1);
-      digitalWrite(LEDB, 0);
+      digitalWrite(RELAY_LED, 0);
     }
     order++;
     if (order >= 8) {
       order = 0;
       index++;
     }
-    if (index >= 1024) {
+    if (index >= end_index) {
       state = 0;
       order = 0;
       index = 0;
       value = 0;
-      digitalWrite(LEDR, 0);
-      digitalWrite(LEDG, 1);
-      digitalWrite(LEDB, 0);
+      end_index = 0;
+      digitalWrite(RECODRD_LED, 0);
+      digitalWrite(TIMER_LED, 0);
+      digitalWrite(NORMAL_LED, 1);
+      digitalWrite(RELAY_LED, 0);
     }
   }
-  if (digitalRead(KEY4) == 0) // Dump data to serial
-  {
-    delay(10);
-    if (digitalRead(KEY4) == 0) {
-      Serial.println("key4");
-      for (int i = 0; i < 1024; i++) {
-        Serial.print(EEPROM.read(i));
-        Serial.print(" ,");
-      }
-    }
-  }
-  // delay(100);
 }
