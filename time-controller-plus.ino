8a81c34986db16b84d12d0928be5c1b69bea6372
dipsy <h920032@gmail.com>
2023-07-16 21:23:48 +0800

    modify

--- time-controller-plus.ino
+++ time-controller-plus.ino
@@ -57,15 +57,221 @@ void setup() {
 
 unsigned long times[2] = {0, 0}; // 記錄運行時間
 unsigned long timestart = 0;
+unsigned long button_time_start = 0;
+unsigned long button_time_end = 0;
 
 int state = 0;
 int index = 0;
 int order = 0;
 int value = 0;
 int end_index = 0;
+int key = 0;
 
-void loop()
-{
+
+void loop() {
+  if (state == 0) {
+    digitalWrite(jdq, 1);
+    digitalWrite(RECODRD_LED, 0);
+    digitalWrite(TIMER_LED, 0);
+    digitalWrite(NORMAL_LED, 1);
+    digitalWrite(RELAY_LED, 0);
+
+    if (digitalRead(KEY1) == 0) // Clearning data
+    {
+      if (key != 1) {
+        button_time_start = millis();
+        button_time_end = millis();
+        key = 1;
+      }
+      else {
+        button_time_end = millis();
+      }
+      // Serial.println("key1");
+      if ((button_time_end - button_time_start) >= 5000)
+      {
+        key = 0;
+        for (int i = 0; i < EEPROM_SIZE; i++) {
+          EEPROM.write(i, 0);
+        }
+        writeIntIntoEEPROM(EEPROM_SIZE - 3, 0);
+        state = 1, index = 0, order = 0, value = 0;
+        digitalWrite(RECODRD_LED, 1);
+        digitalWrite(TIMER_LED, 0);
+        digitalWrite(NORMAL_LED, 0);
+        digitalWrite(RELAY_LED, 0);
+        while (digitalRead(KEY1) == 0);
+        while (digitalRead(KEY2) == 1);
+        times[0] = millis();
+        times[1] = millis();
+      }
+    }
+    else if (digitalRead(KEY2) == 0) // press trigger button
+    {
+      if (key != 2) {
+        button_time_start = millis();
+        button_time_end = millis();
+        key = 2;
+      }
+      else {
+        button_time_end = millis();
+      }
+      if ((button_time_end - button_time_start) >= 10) {
+        key = 0;
+        // Serial.println("key2");
+        if (state == 0)
+        {
+          state = 2, order = 0, index = 0, value = 0;
+          end_index = readIntFromEEPROM(EEPROM_SIZE - 3);
+          times[0] = millis();
+          times[1] = millis();
+        }
+      }
+    }
+    else if (digitalRead(KEY3) == 0) {
+      while (digitalRead(KEY3) == 0) {
+        digitalWrite(jdq, 0);
+        digitalWrite(RELAY_LED, 1);
+      }
+    }
+    else if (digitalRead(KEY4) == 0) // Dump data to serial
+    {
+      /*
+         Enable communicate with serial
+      */
+      if (key != 4) {
+        button_time_start = millis();
+        button_time_end = millis();
+        key = 4;
+      }
+      else {
+        button_time_end = millis();
+      }
+      if ((button_time_end - button_time_start) >= 10) {
+        key = 0;
+        // Serial.println("key4");
+        for (int i = 0; i < MAX_INDEX; i++) {
+          Serial.print(EEPROM.read(i));
+          Serial.print(" ,");
+        }
+      }
+    }
+    else {
+      key = 0;
+    }
+  }
+  else if (state == 1) {
+    if (millis() >= times[0]) //Learning
+    {
+      times[0] += 100;
+      if (millis() >= times[1]) {
+        digitalWrite(TIMER_LED, !digitalRead(TIMER_LED));
+        times[1] += 500;
+      }
+      if (digitalRead(KEY3) == 0)
+      {
+        // Serial.println("key3");
+        value = value ^ (1 << (7 - order));
+        // Serial.print(value);
+        digitalWrite(jdq, 0);
+        digitalWrite(RELAY_LED, 1);
+      }
+      else
+      {
+        digitalWrite(jdq, 1);
+        digitalWrite(RELAY_LED, 0);
+      }
+      order++;
+      if (order >= 8) {
+        EEPROM.write(index, value);
+        order = 0;
+        value = 0;
+        index++;
+        writeIntIntoEEPROM(EEPROM_SIZE - 3, index);
+      }
+    }
+    if (digitalRead(KEY1) == 0  || index >= MAX_INDEX)
+    {
+      if (key != 1) {
+        button_time_start = millis();
+        button_time_end = millis();
+        key = 1;
+      }
+      else {
+        button_time_end = millis();
+      }
+      if ((button_time_end - button_time_start) >= 10  || index >= MAX_INDEX) {
+        key = 0;
+        if (index < MAX_INDEX) {
+          EEPROM.write(index, value);
+          index++;
+          writeIntIntoEEPROM(EEPROM_SIZE - 3, index);
+        }
+        state = 0, order = 0, value = 0, index = 0;
+        end_index = 0;
+        digitalWrite(TIMER_LED, 1);
+        digitalWrite(RELAY_LED, 0);
+        digitalWrite(jdq, 1);
+        while (digitalRead(KEY1) == 0);
+      }
+    }
+    else {
+      key = 0;
+    }
+  }
+  else if (state == 2) {
+    if (millis() >= times[0]) // Trigger
+    {
+      times[0] += 100;
+      if (millis() >= times[1]) {
+        digitalWrite(TIMER_LED, !digitalRead(TIMER_LED));
+        times[1] += 500;
+      }
+      if (order == 0) {
+        value = EEPROM.read(index);
+      }
+      if ((value >> (7 - order)) & 1) {
+        digitalWrite(jdq, 0);
+        digitalWrite(RELAY_LED, 1);
+      }
+      else {
+        digitalWrite(jdq, 1);
+        digitalWrite(RELAY_LED, 0);
+      }
+      order++;
+      if (order >= 8) {
+        order = 0;
+        index++;
+      }
+    }
+    if (digitalRead(KEY1) == 0  || index >= end_index)
+    {
+      if (key != 1) {
+        button_time_start = millis();
+        button_time_end = millis();
+        key = 1;
+      }
+      else {
+        button_time_end = millis();
+      }
+      if ((button_time_end - button_time_start) >= 10  || index >= end_index) {
+        key = 0;
+        state = 0, order = 0, value = 0, index = 0;
+        end_index = 0;
+        digitalWrite(TIMER_LED, 1);
+        digitalWrite(RELAY_LED, 0);
+        digitalWrite(jdq, 1);
+        while (digitalRead(KEY1) == 0);
+      }
+    }
+    else {
+      key = 0;
+    }
+  }
+}
+
+/*
+  void loop()
+  {
   if (digitalRead(KEY1) == 0  || index >= MAX_INDEX) // Clearning data
   {
     delay(10);
@@ -145,9 +351,7 @@ void loop()
   }
   else if (digitalRead(KEY4) == 0 && state == 0) // Dump data to serial
   {
-    /*
-     * Enable communicate with serial
-     */
+       // Enable communicate with serial
     delay(10);
     if (digitalRead(KEY4) == 0) {
       Serial.println("key4");
@@ -221,4 +425,5 @@ void loop()
       digitalWrite(RELAY_LED, 0);
     }
   }
-}
+  }
+*/
