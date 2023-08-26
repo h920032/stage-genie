68792d8fd76c8688812a54a600551b5f6f529302
dipsy <h920032@gmail.com>
2023-08-26 19:34:27 +0800

    update to v3.2

--- stage-genie.ino
+++ stage-genie.ino
@@ -7,12 +7,12 @@
 #define NORMAL_LED 7
 #define RELAY_LED 5
 #define POWER_LED 9
-#define KEY1 A2
-#define KEY2 A1
-#define KEY3 A4
-#define METER A6
+#define KEY1 A1
+#define KEY2 A2
+#define KEY3 A3
+#define METER A4
 #define POWER A5
-#define jdq 2  // Relay control pin
+#define MOS 3  // Relay control pin
 #define INDEX_NUM_ADDR 65536
 #define MAX_INDEX 64000
 #define SPI_PAGESIZE 256
@@ -37,11 +37,9 @@ int key = 0;
 void setup() {
   // Initialize serial communication, configure pins and set initial LED states
   Serial.begin(9600);
-  pinMode(A0, OUTPUT);
-  digitalWrite(A0, 0);
 
-  pinMode(jdq, OUTPUT);
-  digitalWrite(jdq, 0);
+  pinMode(MOS, OUTPUT);
+  digitalWrite(MOS, 0);
   pinMode(RECODRD_LED, OUTPUT);
   pinMode(TIMER_LED, OUTPUT);
   pinMode(NORMAL_LED, OUTPUT);
@@ -126,7 +124,7 @@ void loop() {
         // KEY1 for 5 seconds: Clear flash memory and transition to state 1
         // KEY2: Transition to state 2
         // KEY3: Manual relay control
-        digitalWrite(jdq, 0);
+        digitalWrite(MOS, 0);
         digitalWrite(RECODRD_LED, 0);
         digitalWrite(TIMER_LED, 0);
         digitalWrite(NORMAL_LED, 1);
@@ -189,7 +187,7 @@ void loop() {
           }
         } else if (digitalRead(KEY3) == 0) {
           while (digitalRead(KEY3) == 0) {
-            analogWrite(jdq, map(analogRead(METER), 0, 1023, 0, 255));
+            analogWrite(MOS, map(analogRead(METER), 0, 1023, 0, 255));
             analogWrite(RELAY_LED, map(analogRead(METER), 0, 1023, 0, 255));
           }
         } else {
@@ -211,11 +209,11 @@ void loop() {
             pageBuffer[order] =
               map(analogRead(METER), 0, 1023, 0, 255);  // analogRead(A1) >> 2;
             // Serial.println(pageBuffer[order]);
-            analogWrite(jdq, (int)pageBuffer[order]);
+            analogWrite(MOS, (int)pageBuffer[order]);
             analogWrite(RELAY_LED, (int)pageBuffer[order]);
           } else {
             pageBuffer[order] = 0;
-            analogWrite(jdq, 0);
+            analogWrite(MOS, 0);
             analogWrite(RELAY_LED, 0);
           }
           order++;
@@ -254,7 +252,7 @@ void loop() {
             state = 0, order = 0, value = 0, index = 0;
             end_index = 0;
             // delete timeArray;
-            // digitalWrite(jdq, 0);
+            // digitalWrite(MOS, 0);
             while (digitalRead(KEY1) == 0)
               ;
           }
@@ -276,7 +274,7 @@ void loop() {
             flash.readByteArray(index, &data_buffer[0], SPI_PAGESIZE);
           }
           analogWrite(RELAY_LED, (int)data_buffer[order]);
-          analogWrite(jdq, (int)data_buffer[order]);
+          analogWrite(MOS, (int)data_buffer[order]);
           // Serial.println(data_buffer[order]);
           order++;
           if (order >= SPI_PAGESIZE) {
@@ -302,7 +300,7 @@ void loop() {
             end_index = 0;
             digitalWrite(TIMER_LED, 1);
             digitalWrite(RELAY_LED, 0);
-            digitalWrite(jdq, 0);
+            digitalWrite(MOS, 0);
             while (digitalRead(KEY1) == 0)
               ;
           }
